6dfc522175e27b94d2767e393102159632dd0790
RazvanNasca <73556482+RazvanNasca@users.noreply.github.com>
2023-12-18 22:56:34 +0200

    remove repos page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
-import RepoPage from "./pages/repo/RepoPage";
 
 function App() {
 
@@ -10,7 +9,6 @@ function App() {
       <Routes> {/* used to defined routes in app */}
         <Route path="/" element={<Home />}/> 
         <Route path="/about" element={<About />}/> 
-        <Route path="/:owner/:name" element={<RepoPage />}/> 
       </Routes>
     </BrowserRouter>
   );
